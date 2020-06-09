96df050dbf8b923ef943cf46f54a1ef87ca87d93
brent <brent.e.meyer@gmail.com>
2020-06-09 19:54:43 -0400

    adds missing alt to logo img

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,6 @@ import styles from './header.module.css';
 export const Header = ({ title, src, className }) => (
   <div className={ `${styles.headerWrapper} ${className}` }>
     <h1 className={ styles.title }>{ title }</h1>
-    <img className={ styles.logo } src={ src } />
+    <img className={ styles.logo } src={ src } alt="brent meyer's logo" />
   </div>
 );
