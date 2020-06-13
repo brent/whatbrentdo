5f344d15e74c416a4c7ea878bf903c954a1974b1
brent <brent.e.meyer@gmail.com>
2020-06-13 19:24:37 -0400

    adds home page link to header logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,8 @@ import styles from './header.module.css';
 export const Header = ({ title, src, className }) => (
   <div className={ `${styles.headerWrapper} ${className}` }>
     <h1 className={ styles.title }>{ title }</h1>
-    <img className={ styles.logo } src={ src } alt="brent meyer's logo" />
+    <a href="/">
+      <img className={ styles.logo } src={ src } alt="brent meyer's logo" />
+    </a>
   </div>
 );
