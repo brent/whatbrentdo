0bdc6ecc8c803344279db86258402cd7e4321b35
Brent Meyer <brent.e.meyer@gmail.com>
2025-04-18 23:00:04 -0400

    adds posthog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,24 @@
   import Header from '$lib/components/Header'
   import Footer from '$lib/components/Footer'
   import type { Snippet } from 'svelte'
+  import posthog from 'posthog-js'
+  import { browser } from '$app/environment'
+  import { onMount } from 'svelte'
 
   type Props = { children: Snippet }
   const { children }: Props = $props()
+
+  onMount(() => {
+    if (browser) {
+      posthog.init(
+        'phc_IAjkQ3XlrzuDXFWcQAa07bs8kiz8SC9w3BvnDMqNPeO',
+        {
+          api_host: 'https://us.i.posthog.com',
+          person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+        }
+      )
+    }
+  })
 </script>
 
 <div class="wrapper">
